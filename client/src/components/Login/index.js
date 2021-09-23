b72066107bc5bf157ed1a17d58bedbad32249640
DungNNb <tienphongcutin@gmail.com>
2021-09-23 22:18:34 +0700

    Update login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -4,43 +4,40 @@ import './styles/index.css';
 import {
     Link
 } from "react-router-dom";
-import logo from './styles/images/logo.png';
 
 function Login(props) {
     return <div className="login">
-            <div className="row">
-                <div className="col-6">
-                    {/*<div className='social'>
-                        <img src={logo} alt="" />   
-                    </div>*/}
-                    <div className="form-login">
-                        <div style={{display: 'flex'}}>
-                            <h1>Đăng Nhập</h1>
-                        </div>
-                        <div className="form">
-                            <b>Tài Khoản</b>
-                            <input type="text" placeholder="Nhập tài khoản" />
-                            <b>Mật Khẩu</b>
-                            <input type="password" placeholder="Nhập mật khẩu" />
-                            {/* <div><input type="checkbox"/> <p>Remember me</p></div> */}
-                            <div style={{marginBottom: 10}}>
-                                <input className="inp-cbu" id="rememberAccount" type="checkbox" value="rememberAccount" name='levelai' />
-                                <label className="cbu" for="rememberAccount"><span>
-                                    <svg width="12px" height="9px" viewbox="0 0 12 9">
-                                        <polyline points="1 5 4 8 11 1"></polyline>
-                                    </svg></span>
-                                </label>
-                                <span>Ghi nhớ đăng nhập</span>
-                            </div>
-                            <Link to="/">
-                                <button className="button-login">Đăng Nhập</button>
-                            </Link>
-                        </div>
-                    </div>
+    <div className="row">
+        <div className="col-6">
+            <div className="form-login">
+                <div style={{display: 'flex'}}>
+                    <h1>Login</h1>
                 </div>
-                <div className="col-6 login-img"></div>
+                <div className="form">
+                    <p className="or-sign-in-email">---------- or sign in with email ----------</p>
+                    <b>Account:</b>
+                    <input type="text" placeholder="Username or your email" />
+                    <b>Password:</b>
+                    <input type="password" placeholder="your password"/>
+                    {/* <div><input type="checkbox"/> <p>Remember me</p></div> */}
+                    <div>
+                        <input className="inp-cbu" id="rememberAccount" type="checkbox" value="rememberAccount" name='levelai' />
+                        <label className="cbu" for="rememberAccount"><span>
+                            <svg width="12px" height="9px" viewbox="0 0 12 9">
+                                <polyline points="1 5 4 8 11 1"></polyline>
+                            </svg></span>
+                        </label>
+                        <span>Remember me</span>
+                    </div>
+                    <a href="/home">
+                        <button className="button-login">Login</button>
+                    </a>
+                </div>
             </div>
         </div>
+        <div className="col-6 login-img"></div>
+    </div>
+</div>
 }
 
 Login.propTypes = {
